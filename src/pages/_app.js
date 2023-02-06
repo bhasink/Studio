7e764f473c1cab133af3f6b05ca062872483a9a0
bhasink <bhasink69@gmail.com>
2023-02-06 21:49:33 +0530

    Updated dev data of work page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
+import { RemoveScrollBar } from 'react-remove-scroll-bar'
 
 function Loading() {
   const router = useRouter()
@@ -31,71 +32,26 @@ function Loading() {
 
   return (
     loading && (
-      <div className="spinner-wrapper">
-        <div className="sub-lg">
-          <div className="lgo">
-            <img
-              src="/images/logo.svg"
-              className="navbar-brand-img"
-              alt="logo"
-            />
-            <span></span>
+      <>
+        <RemoveScrollBar />
+        <div className="spinner-wrapper">
+          <div className="sub-lg">
+            <div className="lgo">
+              <img
+                src="/images/logo.svg"
+                className="navbar-brand-img"
+                alt="logo"
+              />
+              <span></span>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     )
   )
 }
 
 function MyApp({ Component, pageProps }) {
-  // const router = useRouter();
-
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() =>{
-  //     setTimeout(() => setLoading(false), 1000);
-  // })
-
-  // useEffect(() => {
-
-  //     const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-  //     const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},1000);
-
-  //     router.events.on('routeChangeStart', handleStart)
-  //     router.events.on('routeChangeComplete', handleComplete)
-  //     router.events.on('routeChangeError',  handleComplete)
-
-  //     return () => {
-  //         router.events.off('routeChangeStart', handleStart)
-  //         router.events.off('routeChangeComplete', handleComplete)
-  //         router.events.off('routeChangeError', handleComplete)
-  //     }
-  // })
-
-  //     return (<>
-
-  //     {loading == true ? (
-  //         <div className='spinner-wrapper'>
-  //                <div className='sub-lg'>
-  //                 <div className='lgo'>
-  //                 <img src="/images/logo.svg" className="navbar-brand-img" alt="logo" />
-
-  //                 <span>
-  //                 </span>
-
-  //                 </div>
-  //                 </div>
-  // </div>
-  //           ) :
-  //     (
-  //         <>
-  //    <Component {...pageProps} />
-
-  //         </>
-  //     )}
-
-  //     </>)
-
   return (
     <>
       <Loading />
